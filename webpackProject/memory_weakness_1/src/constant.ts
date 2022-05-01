3facdbf1499bbb9ee11fa5c73fddf3d284fa1cf8
ReoHosokawa <story.0206.0803@gmail.com>
2022-05-01 14:40:20 +0900

    処理に必要な定義（定数）の追加

--- webpackProject/memory_weakness_1/src/constant.ts
+++ webpackProject/memory_weakness_1/src/constant.ts
@@ -3,6 +3,35 @@
  * 定数クラス
  */
 export class Constant {
+
+    /**
+     * 最大ペア数
+     */
+    static get MaxPair(): number {
+        return 10;
+    }
+
+    /**
+     * トランプの画像（ジョーカー）を示す番号
+     */
+    static get JokerNumber(): number {
+        return 14;
+    }
+
+    /**
+     * トランプ画像の開始番号
+     */
+    static get StartCardNumber(): number {
+        return 1;
+    }
+
+    /**
+     * トランプ画像の終了番号
+     */
+    static get EndCardNumber(): number {
+        return 14;
+    }
+
     /**
      * 画像フォルダのパス
      */
@@ -11,10 +40,17 @@ export class Constant {
     }
 
     /**
+     * トランプ画像の拡張子
+     */
+    static get ImageExtension(): string {
+        return ".gif";
+    }
+
+    /**
      * 裏向きトランプ画像のファイル名
      */
     static get DefaultCardFileName(): string {
-        return "z02.gif";
+        return "z02";
     }
 
     /**
@@ -36,5 +72,18 @@ export class Constant {
      */
     static get LifeImageClassName(): string {
         return "lifeImage";
+    }
+
+    /**
+     * カードの絵札（スペード、ハート、ダイヤ、クラブ、ジョーカー）別 key と value の一覧
+     */
+    static get CardTypeList() {
+        return {
+            Spade: { "key": "s", "value": 0 },
+            Heart: { "key": "h", "value": 1 },
+            Diamond: { "key": "d", "value": 2 },
+            Club: { "key": "c", "value": 3 },
+            Joker: { "key": "x", "value": 4 },
+        };
     }
 }

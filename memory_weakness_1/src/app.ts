6177eed92c0ec00a4de84d45d9f33ded92896a3c
reo.hosokawa <story.0206.0803@gmail.com>
2022-09-19 14:22:04 +0900

    タッチ対応デバイスかどうかに応じて表示文言を変更するように処理を修正

--- memory_weakness_1/src/app.ts
+++ memory_weakness_1/src/app.ts
@@ -2,12 +2,24 @@ import { MemoryWeakness } from './memory_weakness';
 import "./scss/index.scss";
 
 
+/**
+ * タッチ対応デバイスかどうか
+ * @returns タッチ操作可能なら true 、それ以外は false
+ */
+const isTouchDevice = () => typeof window.ontouchstart === "object";
+
 const appInit = () => {
     const memoryWeakness = new MemoryWeakness();
     memoryWeakness.init();
 
     // スマホで操作時、ダブルタップで拡大してしまうのを防止する
     document.addEventListener("dblclick", (e: MouseEvent) => e.preventDefault(), { passive: false });
+
+    // タッチ対応デバイスかどうかに応じて、表示内容を切り替える
+    const clickTypeValue = isTouchDevice() ? "タップ" : "クリック";
+
+    const $subtitleArea = <HTMLDivElement>document.getElementById("subtitleArea");
+    $subtitleArea.textContent = `カードをめくるときはダブル${clickTypeValue}！！`;
 }
 
 window.onload = appInit;

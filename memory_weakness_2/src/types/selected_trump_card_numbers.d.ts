127160433d30557ab06da006aec5547fc0d8e64e
ReoHosokawa <94281946+ReoHosokawa@users.noreply.github.com>
2024-03-25 01:11:59 +0900

    「選択中のカード情報初期値」の定数定義を追加

---
+++ memory_weakness_2/src/types/selected_trump_card_numbers.d.ts
@@ -0,0 +1,15 @@
+
+/**
+ * 選択中のトランプカード情報
+ */
+export interface SelectedTrumpCardNumbers {
+    /**
+     * 1 枚目に選択したカード番号
+     */
+    first: number;
+
+    /**
+     * 2 枚目に選択したカード番号
+     */
+    second: number;
+}

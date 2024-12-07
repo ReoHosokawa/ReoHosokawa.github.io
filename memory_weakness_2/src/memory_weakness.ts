44b1ec714d49eaa6fc7fbdb4011f3b79d8c61a3a
ReoHosokawa <94281946+ReoHosokawa@users.noreply.github.com>
2024-12-07 23:21:16 +0900

    common_processing.ts に定義されていた関数を memory_weakness.ts に移動した
    それに合わせて common_processing.ts ファイルを削除した

--- memory_weakness_2/src/memory_weakness.ts
+++ memory_weakness_2/src/memory_weakness.ts
@@ -37,6 +37,20 @@ let pairCount: number;
  */
 let missCount: number;
 
+/**
+ * 指定された数値の先頭を 0 埋めする
+ * @param target 対象の数値
+ * @param len 0 埋めする桁数
+ * @returns 0 埋めした後の値
+ */
+const zeroPadding = (target: number, length: number): string => String(target).padStart(length, "0");
+
+/**
+ * 指定された時間、処理を停止する
+ * @param ms 処理を停止する時間（ミリ秒）
+ * @returns なし
+ */
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * 選択されたカード番号に紐づくトランプの画像パスを生成する
